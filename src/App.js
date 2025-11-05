bd2248f89bd0225920b087868fd3d00bcb367b75
mwbintang <bintangmuhammadwahid@gmail.com>
2025-11-05 23:04:12 +0700

    chore: initialize React admin dashboard project setup with Tailwind and React Router

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-600">Admin Dashboard</h1>
+  );
+}
+
+export default App;
